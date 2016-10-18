54fe367d055c66c6c3c96c1476e171be68871a75
kikkoooo <kikko@youvsjesus.com>
2016-10-17 21:24:09 -0400

    reconfigured demo files

--- scripts/app.js
+++ scripts/app.js
@@ -3,24 +3,21 @@ $(document).ready(function(){
 	// Get JSON file
 	$.ajax({
 		type: 'GET',
-		url: 'scripts/data.json',
+		url: './scripts/data.json',
+		dataType: 'json',
 		success: function(data) {
-			$.each(data, function(i, player){
-				$("#message").append(player.number + ": " + player.name + "<br/>");
-		    });
-		}
-	});
 
-	// Get XML file
-	$.ajax({
-		type: 'GET',
-		url: 'scripts/data.xml',
-		success: function(xml) {
-			$(xml).find('player').each(function(){
-				var number = $(this).find('number').text();
-				var name = $(this).find('name').text();
-				$("#message").append(number + ": " + name + "<br/>");
-			});
+			$.each(data, function(i, player){
+
+				if (player.faveColor == "red") {
+					$("#message").append('<div class="player red">'+player.name + "</div>");
+				} else if (player.faveColor == "blue") {
+					$("#message").append('<div class="player orange">'+player.name + "</div>");					
+				} else if (player.faveColor == "orange") {
+					$("#message").append('<div class="player blue">'+player.name + "</div>");					
+				}
+		    });
+
 		}
 	});
 
