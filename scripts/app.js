eb8806fd8b01108a6257f502b93468917b8a78c5
kikkoooo <kikko@youvsjesus.com>
2016-10-19 20:25:08 -0400

    added json-test

--- scripts/app.js
+++ scripts/app.js
@@ -3,19 +3,30 @@ $(document).ready(function(){
 	// Get JSON file
 	$.ajax({
 		type: 'GET',
-		url: './scripts/data.json',
+		//url: './scripts/data.json',
+		//url: './scripts/data-test.json', 
 		dataType: 'json',
 		success: function(data) {
 
 			$.each(data, function(i, player){
 
-				if (player.faveColor == "red") {
-					$("#message").append('<div class="player red">'+player.name + "</div>");
-				} else if (player.faveColor == "blue") {
-					$("#message").append('<div class="player orange">'+player.name + "</div>");					
-				} else if (player.faveColor == "orange") {
-					$("#message").append('<div class="player blue">'+player.name + "</div>");					
-				}
+
+
+				faveColorsArray = player.faveColor.split(",");
+				// $.each(faveColorsArray, function(index, value) { 
+				// 	alert(index + ': ' + value);
+				// });
+
+
+				$("#message").append('<div class="player red">'+faveColors[0] + "</div>");
+
+				// if (player.faveColor == "red") {
+				// 	$("#message").append('<div class="player red">'+player.name + "</div>");
+				// } else if (player.faveColor == "blue") {
+				// 	$("#message").append('<div class="player orange">'+player.name + "</div>");					
+				// } else if (player.faveColor == "orange") {
+				// 	$("#message").append('<div class="player blue">'+player.name + "</div>");					
+				// }
 		    });
 
 		}
